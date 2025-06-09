14c99f61af825cdb73b0ff80d6aa690efdf2b64a
krismasdev <oliverescriotino8gmail.com>
2025-06-09 13:36:07 -0400

    fix error

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 // Dynamic imports for ES modules
 let registerRoutes, dotenv;
@@ -102,7 +102,7 @@ async function createApp() {
   return app;
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   try {
     console.log(`${req.method} ${req.url}`);
     const app = await createApp();
@@ -121,4 +121,4 @@ module.exports = async function handler(req, res) {
       });
     }
   }
-}; 
+} 
